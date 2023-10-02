324bc710639f8869d918821dd7798fa365107c59
Petrik Diak <diak@petrik.hu>
2023-10-02 15:39:31 +0200

    4 bemeneti mezo megvalositása

--- main.js
+++ main.js
@@ -1,3 +1,36 @@
 import './style.css'
 import 'bootstrap/dist/css/bootstrap.css' 
 
+document.addEventListener('DOMContentLoaded',tolt)
+
+function tolt()
+{
+  //document.getElementById('gombID').addEventListener('click',katt);
+  document.getElementById('gombID').addEventListener('click',kepSzelesseg);
+  document.getElementById('gombID').addEventListener('click',kepKeretVastagsag);
+  document.getElementById('gombID').addEventListener('click',urlBealittas);
+}
+
+function urlBealittas()
+{
+  let url = document.getElementById('urlID').value;
+  console.log(url);
+  document.getElementById('kepID').src = url;
+}
+function kepSzelesseg()
+{
+  let a = document.getElementById('szelessegID').value;
+  document.getElementById('kepID').style.width= a+"px";
+  document.getElementById('kepID').style.height= a+"px";
+
+
+  
+}
+function kepKeretVastagsag()
+{
+  let vastagsag = document.getElementById('vastagsagID').value;
+  let szin = document.getElementById('szinID').value;
+  let string = `${vastagsag}px solid ${szin}`;
+  document.getElementById("kepID").style.border = string;
+  console.log(string);
+}
